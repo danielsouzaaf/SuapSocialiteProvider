c88c0bf428393a47f61731bcd3cc71745bf88356
DraperStudio <hello@draperstud.io>
2015-03-07 11:59:27 +0100

    Update comments

--- src/HerokuExtendSocialite.php
+++ src/HerokuExtendSocialite.php
@@ -6,7 +6,9 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 class HerokuExtendSocialite
 {
     /**
-     * Execute the provider.
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
      */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
